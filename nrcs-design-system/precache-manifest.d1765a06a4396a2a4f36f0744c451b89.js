92f7d569d3ce7d8fecc90e816790f7d7a8c3ae28
Ian Erickson <eddieshowcase@gmail.com>
2020-02-20 13:42:28 -0700

    Initial WIP commit of nrcs-design-system storybook-static site.

---
+++ nrcs-design-system/precache-manifest.d1765a06a4396a2a4f36f0744c451b89.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0616f6e518fa2d2a7db3783f48d32556",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.aa0d616c8ab2ca691f15.bundle.js"
+  },
+  {
+    "url": "runtime~main.aa0d616c8ab2ca691f15.bundle.js"
+  },
+  {
+    "url": "vendors~main.aa0d616c8ab2ca691f15.bundle.js"
+  },
+  {
+    "url": "vendors~main.aa0d616c8ab2ca691f15.bundle.js.LICENSE.txt"
+  }
+]);
