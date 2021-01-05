342fcdfbf5a083bd9cc6f3a6ac532dc56aca20e6
Ian Erickson <eddieshowcase@gmail.com>
2021-01-05 16:32:59 -0700

    try to fix nested routes with workaround for basename.

--- nrcs-design-system/precache-manifest.eb1ca9a259ecd8079cad243e9f2f68ef.js
+++ nrcs-design-system/precache-manifest.eb1ca9a259ecd8079cad243e9f2f68ef.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4fdb5c95e48ddb80b83d86cc43b5eb04",
+    "revision": "103afe4a8888ba89ca59fd40b0feb790",
     "url": "/nrcs-design-system/index.html"
   },
   {
-    "revision": "045e83a1820ce6138d52",
+    "revision": "7ad70b977ace4df55b1a",
     "url": "/nrcs-design-system/static/css/main.21143e9f.chunk.css"
   },
   {
-    "revision": "147c1d5ef574c6a5038d",
-    "url": "/nrcs-design-system/static/js/2.19ecab72.chunk.js"
+    "revision": "da4d3f41cdfa3219bb90",
+    "url": "/nrcs-design-system/static/js/2.5ef32864.chunk.js"
   },
   {
     "revision": "4145be489bd01885e4708838374693cb",
-    "url": "/nrcs-design-system/static/js/2.19ecab72.chunk.js.LICENSE.txt"
+    "url": "/nrcs-design-system/static/js/2.5ef32864.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "045e83a1820ce6138d52",
-    "url": "/nrcs-design-system/static/js/main.21cc31c1.chunk.js"
+    "revision": "7ad70b977ace4df55b1a",
+    "url": "/nrcs-design-system/static/js/main.aa99202f.chunk.js"
   },
   {
     "revision": "7c024a03f3a9261cc7d3",
--- nrcs-design-system/service-worker.js
+++ nrcs-design-system/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nrcs-design-system/precache-manifest.c9dd04c3d6138cebc377f45b117ee848.js"
+  "/nrcs-design-system/precache-manifest.eb1ca9a259ecd8079cad243e9f2f68ef.js"
 );
 
 self.addEventListener('message', (event) => {
